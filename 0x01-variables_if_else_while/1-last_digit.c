03dc29371840b9c19702b4e4c211008f800d28b7
Thelastdroid <slinzomashwama@gmail.com>
2023-03-19 09:46:01 -0700

    This program will assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 /**
  *main - main function
  *
- *Return : 0 is positive
+ *Return: 0 is positive
  */
 int main(void)
 {
@@ -18,10 +18,11 @@ int main(void)
 	/* your code goes there */
 	m = n % 10;
 	if (m > 5)
-		printf("Lastdigit of %d is %d and is greater than 5", n, m);
+		printf("Last digit of %d is %d and is greater than 5", n, m);
 	else if (m == 0)
 		printf("Last digit of %d is %d and is 0", n, m);
 	else if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
+
 	return (0);
 }
